723133541f9eb87aaeff2a0f49ab3227b22479a3
Aliaksei Bakhur <a.bakhur@godeltech.com>
2019-12-30 16:54:22 +0300

    Updates

---
+++ precache-manifest.9c202453b698d7c95ee1f4cbb8b7c2e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b99e8b4a60802cdbae42bffe2faba19b",
+    "url": "/react_developer/index.html"
+  },
+  {
+    "revision": "df86a7545643e4087bde",
+    "url": "/react_developer/static/css/main.62f665c1.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/react_developer/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react_developer/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "df86a7545643e4087bde",
+    "url": "/react_developer/static/js/main.c67ebc55.chunk.js"
+  },
+  {
+    "revision": "197c58192cbb4d0d7207",
+    "url": "/react_developer/static/js/runtime-main.ff68e8bd.js"
+  }
+]);
